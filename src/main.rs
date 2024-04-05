45012f2b0646ea7b86d65a7e3396b403a9721a9e
AliBakerSartawi <alisartawi@live.com>
2024-04-05 15:59:08 +0300

    init shell

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::io::{stdin, stdout, Write};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to the Kiwi 🥝 shell!");
+    println!("Type 'help' to see a list of commands");
+
+    shell();
+}
+
+fn shell() {
+    loop {
+        print!("> ");
+        stdout().flush().unwrap();
+        
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
+        let input = input.trim();
+        
+        if input == "exit" || input == "quit" {
+            break;
+        }
+
+        handle_command(input);
+    }
+}
+
+fn handle_command(input: &str) {
+    // TODO: empty command or `help` shows help
+
+    println!("You entered: {}", input);
 }
