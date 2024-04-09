45b111d25b1dbcf566f0f77bc0f77a2a827065d2
AliBakerSartawi <alisartawi@live.com>
2024-04-09 18:23:24 +0300

    get type from key name

--- src/parser.rs
+++ src/parser.rs
@@ -1,3 +1,5 @@
+use std::str::SplitWhitespace;
+
 use crate::store::{Key, Value};
 
 pub enum Command {
@@ -9,55 +11,79 @@ pub enum Command {
     Empty,
 }
 
+#[derive(Debug)]
+pub enum ValueType {
+    Str,
+    Int,
+    Float,
+    Bool,
+}
+
 pub fn parse_input(input: String) -> Result<Command, String> {
-    let input = input.trim();
-    let mut parts = input.split_whitespace();
+    let mut parts = input.trim().split_whitespace();
 
     let command_case_insensitive = parts.next().map(|s| s.to_lowercase());
 
     match command_case_insensitive.as_deref() {
-        Some("set") => {
-            let value_type = parts.next().ok_or("No value type provided")?.to_lowercase();
-
-            let key = parts.next().ok_or("No key provided")?;
-            let value = parts.next().ok_or("No value provided. Or did you forget to provide the type of the value, e.g. `set <type> <key> <value>`")?;
-
-            let typed_value = match value_type.as_str() {
-                "str" => Value::Str(value.to_string()),
-                "int" => value
-                    .parse::<i64>()
-                    .map(Value::Int)
-                    .map_err(|_| "Invalid integer format for value")?,
-                "float" => value
-                    .parse::<f64>()
-                    .map(Value::Float)
-                    .map_err(|_| "Invalid float format for value")?,
-                "bool" => match value.to_lowercase().as_str() {
-                    // Also handle boolean value case-insensitively
-                    "true" => Value::Bool(true),
-                    "false" => Value::Bool(false),
-                    _ => return Err("Invalid boolean value; expected true or false".into()),
-                },
-                _ => return Err(format!("Invalid value type: {}", value_type)),
-            };
-
-            Ok(Command::Set(key.to_string(), typed_value))
-        }
-
-        Some("get") => {
-            let key = parts.next().ok_or("No key provided")?.to_string();
-            Ok(Command::Get(key))
-        }
-
-        Some("del") => {
-            let key = parts.next().ok_or("No key provided")?.to_string();
-            Ok(Command::Del(key))
-        }
-
-        Some("help") => Ok(Command::Help),
-
-        Some(cmd) => Ok(Command::Unknown(cmd.to_string())),
-
+        Some("set") => parse_set_command(&mut parts),
+        Some("get") => parse_get_command(&mut parts),
+        Some("del") => parse_del_command(&mut parts),
+        Some("help") => parse_help_command(),
+        Some(cmd) => parse_unknown_command(cmd),
         None => Ok(Command::Empty),
     }
+}
+
+fn parse_set_command(parts: &mut SplitWhitespace<'_>) -> Result<Command, String> {
+    let key = parts.next().ok_or("No key provided")?;
+    let value_type = match key.split('-').next() {
+        Some(t) => match t.to_lowercase().as_str() {
+            "str" => ValueType::Str,
+            "int" => ValueType::Int,
+            "float" => ValueType::Float,
+            "bool" => ValueType::Bool,
+            _ => ValueType::Str,
+        },
+        None => ValueType::Str,
+    };
+    let value = parts.next().ok_or("No value provided")?;
+
+    let typed_value = match value_type {
+        ValueType::Str => Value::Str(value.to_string()),
+        ValueType::Int => value
+            .parse::<i64>()
+            .map(Value::Int)
+            .map_err(|_| format!("{value} cannot be parsed as an integer"))?,
+        ValueType::Float => value
+            .parse::<f64>()
+            .map(Value::Float)
+            .map_err(|_| format!("{value} cannot be parsed as a float"))?,
+        ValueType::Bool => match value.to_lowercase().as_str() {
+            // Also handle boolean value case-insensitively
+            "true" => Value::Bool(true),
+            "false" => Value::Bool(false),
+            _ => return Err("Invalid boolean value; expected true or false".into()),
+        },
+        _ => return Err(format!("Invalid value type: {:?}", value_type)),
+    };
+
+    Ok(Command::Set(key.to_string(), typed_value))
+}
+
+fn parse_get_command(parts: &mut SplitWhitespace<'_>) -> Result<Command, String> {
+    let key = parts.next().ok_or("No key provided")?.to_string();
+    Ok(Command::Get(key))
+}
+
+fn parse_del_command(parts: &mut SplitWhitespace<'_>) -> Result<Command, String> {
+    let key = parts.next().ok_or("No key provided")?.to_string();
+    Ok(Command::Del(key))
+}
+
+fn parse_help_command() -> Result<Command, String> {
+    Ok(Command::Help)
+}
+
+fn parse_unknown_command(cmd: &str) -> Result<Command, String> {
+    Ok(Command::Unknown(cmd.to_string()))
 }
